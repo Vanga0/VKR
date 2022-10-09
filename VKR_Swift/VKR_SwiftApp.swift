cb913bfc77d37a4c45d249af1a608012018f2683
Vagan <vagan.albertyan@yandex.ru>
2022-10-09 12:17:38 +0300

    Initial Commit

---
+++ VKR_Swift/VKR_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  VKR_SwiftApp.swift
+//  VKR_Swift
+//
+//  Created by Vagan Albertyan on 09.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct VKR_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
